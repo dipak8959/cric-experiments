30629c5db70778f4135e8b41d6954f0b04ccec7f
Dipak Patel <dipak8959@gmail.com>
2020-09-22 18:22:59 -0400

    Updates

---
+++ precache-manifest.b63e771d9098d1a17eed37ed8c330c9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ca1b9967c392f24f3b88a3c99c7e07",
+    "url": "/cric-experiments/index.html"
+  },
+  {
+    "revision": "19f3a06258d1c809290b",
+    "url": "/cric-experiments/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edd17c1e44cde2e74973",
+    "url": "/cric-experiments/static/js/2.c7fc1303.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cric-experiments/static/js/2.c7fc1303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f3a06258d1c809290b",
+    "url": "/cric-experiments/static/js/main.cfa0ffb1.chunk.js"
+  },
+  {
+    "revision": "a0288caaf1d9955b0794",
+    "url": "/cric-experiments/static/js/runtime-main.c53390f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cric-experiments/static/media/logo.5d5d9eef.svg"
+  }
+]);
